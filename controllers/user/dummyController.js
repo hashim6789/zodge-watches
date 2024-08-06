659b6e4d5cedfa4b82aa17a8a7a317ed5264c467
hashim6789 <muhammedhashim6789@gmail.com>
2024-08-06 15:29:58 +0530

    The signup and otp features are sttled

--- controllers/user/dummyController.js
+++ controllers/user/dummyController.js
@@ -2,11 +2,11 @@ const ProductModel = require("../../models/ProductModel");
 const CategoryModel = require("../../models/Category");
 
 const dummy = async (req, res) => {
-  // const products = await ProductModel.find({ isListed: true });
-  // const categories = await CategoryModel.find({ isListed: true });
-  // console.log(categories);
+  const products = await ProductModel.find({ isListed: true });
+  const categories = await CategoryModel.find({ isListed: true });
+  console.log(categories);
 
-  // res.render("user/home", { products, categories });
+  res.render("user/home", { products, categories });
   res.render("user/otpGeneratePage", { msg: null });
 };
 
--- routes/user/authRoute.js
+++ routes/user/authRoute.js
@@ -1,17 +1,34 @@
 const express = require("express");
+
 const {
   getSignup,
   postSignup,
+  getOtpPage,
+  postOtp,
   getLogin,
   postLogin,
+  getHome,
 } = require("../../controllers/user/authController");
 const router = express.Router();
+
+const test = (req, res) => {
+  console.log(req.url);
+};
 
 //get the signup page
 router.get("/signup", getSignup);
 
 //post the signup page
 router.post("/signup", postSignup);
+
+//get otp entering page
+router.get("/verify-otp", getOtpPage);
+
+//post otp generator
+router.post("/verify-otp", postOtp);
+
+//get home page
+router.get("/home", getHome);
 
 //get the login page
 router.get("/login", getLogin);
