8d2cc402dc412df47f561666bc0a94c78b59f104
hashim6789 <muhammedhashim6789@gmail.com>
2024-10-01 16:59:49 +0530

    The db error

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 // MongoDB URI from .env file
-const mongoDbUri = process.env.mongoDbUri;
+const mongoDbUri =
+  process.env.mongoDbUri || "mongodb://localhost:27017/zodgeDB";
 
 // Log the URI to verify it's correct
 console.log("MongoDB URI:", mongoDbUri);
